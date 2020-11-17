71e2a0080a438e98aa7afe4493a0973714f3618e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-11-17 12:16:11 +0100

    Command

--- src/DesignPatterns/CommandPattern/Program.cs
+++ src/DesignPatterns/CommandPattern/Program.cs
@@ -1,7 +1,27 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CommandPattern
 {
+
+    // UnitOfWork
+
+    //public class CommandsUnitOfWork
+    //{
+    //    private Queue<ICommand> commands = new Queue<ICommand>();
+
+    //    public void Add(ICommand command)
+    //    {
+    //        commands.Enqueue(command);
+    //    }
+
+    //    public void Print()
+    //    {
+
+    //    }
+    //}
+
     class Program
     {
         static void Main(string[] args)
@@ -10,15 +30,26 @@ namespace CommandPattern
 
             Message message = new Message("555000123", "555888000", "Hello World!");
 
-            if (message.CanPrint())
+            Queue<ICommand> commands = new Queue<ICommand>();
+
+            commands.Enqueue(new SendCommand("777777", "645654654", message.Content));
+            commands.Enqueue(new PrintCommand(3, "423423432", "645654654", message.Content));
+
+            commands.Enqueue(new SendCommand("777777", "645654654", message.Content));
+            commands.Enqueue(new PrintCommand(4, "777777", "645654654", message.Content));
+            
+
+            while (commands.Count > 0)
             {
-                message.Print();
+                ICommand command = commands.Dequeue();
+
+                if (command.CanExecute())
+                {
+                    command.Execute();
+                }
             }
 
-            if (message.CanSend())
-            {
-                message.Send();
-            }    
+            
         }
     }
 
@@ -66,4 +97,66 @@ namespace CommandPattern
     }
 
     #endregion
+
+    // Abstract Command
+    public interface ICommand
+    {
+        void Execute();
+        bool CanExecute();
+    }
+
+    // Concrete Command
+    public class SendCommand : ICommand
+    {
+        public SendCommand(string from, string to, string content)
+        {
+            this.from = from;
+            this.to = to;
+            this.content = content;
+        }
+
+        private readonly string from;
+        private readonly string to;
+        private readonly string content;
+
+        public void Execute()
+        {            
+            Console.WriteLine($"Send message from <{from}> to <{to}> {content}");
+        }
+
+        public bool CanExecute()
+        {
+            return !(string.IsNullOrEmpty(from) || string.IsNullOrEmpty(to) || string.IsNullOrEmpty(content));
+        }
+    }
+
+    // Concrete Command
+    public class PrintCommand : ICommand
+    {
+        private readonly int copies;
+        private readonly string content;
+        private readonly string from;
+        private readonly string to;
+
+        public PrintCommand(int copies, string content, string from, string to)
+        {
+            this.copies = copies;
+            this.content = content;
+            this.from = from;
+            this.to = to;
+        }
+
+        public bool CanExecute()
+        {
+            return !string.IsNullOrEmpty(content);
+        }
+
+        public void Execute()
+        {
+            for (int i = 0; i < copies; i++)
+            {
+                Console.WriteLine($"Print message from <{from}> to <{to}> {content}");
+            }
+        }
+    }
 }

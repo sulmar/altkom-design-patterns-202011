c916fe67bb0cafa23828defc91d4c823df8fb88b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-11-18 11:19:15 +0100

    NullObject

--- src/DesignPatterns/NullObjectPattern/Program.cs
+++ src/DesignPatterns/NullObjectPattern/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace NullObjectPattern
 {
@@ -10,38 +13,71 @@ namespace NullObjectPattern
 
             IProductRepository productRepository = new FakeProductRepository();
 
-            Product product = productRepository.Get(1);
+            ProductBase product = productRepository.Get(9);
 
             // Problem: Zawsze musimy sprawdzać czy obiekt nie jest pusty (null).
 
-            if (product != null)
-            {
+            //if (product != null)
+            //{
                 product.RateId(3);
-            }
+            //}
         }
     }
 
     public interface IProductRepository
     {
-        Product Get(int id);
+        ProductBase Get(int id);
     }
 
     public class FakeProductRepository : IProductRepository
     {
-        public Product Get(int id)
+        private readonly IEnumerable<ProductBase> products = new Collection<ProductBase>();
+
+        public FakeProductRepository()
         {
-            return null;
+            products = new Collection<ProductBase>
+            {
+                new Product { Id = 1 },
+                new Product { Id = 2 },
+                new Product { Id = 3 },
+            };
+        }
+
+        public ProductBase Get(int id)
+        {
+            ProductBase product = products.SingleOrDefault(p=> p.Id == id);
+
+            if (product == null)
+                product = new NullProduct();
+
+            return product;
         }
     }
 
-    public class Product
+    public abstract class ProductBase
     {
+        public int Id { get; set; }
+        public abstract void RateId(int rate);
+    }
+
+    public class Product : ProductBase
+    {
+       
         private int rate;
 
-        public void RateId(int rate)
+        public override void RateId(int rate)
         {
             this.rate = rate;
         }
 
+    }
+
+    // NullObject
+    public class NullProduct : ProductBase
+    {
+        public override void RateId(int rate)
+        {
+            // nic nie rob
+        }
     }
 }
